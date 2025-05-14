8777f70b3ea5b1185bbc0b830224e3eada9b7f8e
Alexi GRIGOROV <alexigrigorov@gmail.com>
2025-05-14 05:40:49 +0200

    bump version to 0.1.2

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,4 @@
 import Modal from "./Modal";
-import Dialog from "./Dialog";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
@@ -7,7 +6,6 @@ import CloseButton from "./CloseButton";
 import Overlay from "./Overlay";
 
 // Attach compound slots
-Modal.Dialog = Dialog;
 Modal.Header = Header;
 Modal.Body = Body;
 Modal.Footer = Footer;
